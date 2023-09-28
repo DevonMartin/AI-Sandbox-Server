9f8d1aef42e06ef463c4636b81290d3451f61c15
Devon Martin <devon.martin.re@gmail.com>
2023-09-28 17:12:56 -0400

    Testing RevenueCat BS

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,11 +7,9 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 	
-//	guard let databaseURL = Environment.get("DATABASE_URL") else {
-//		fatalError("Unable to retrieve database URL from the environment.")
-//	}
-	
-	app.databases.use(try .postgres(url: "postgres://postgres@localhost:5432/devon"), as: .psql)
+	guard let databaseURL = Environment.get("DATABASE_URL") else {
+		fatalError("Unable to retrieve database URL from the environment.")
+	}
 	
 	app.migrations.add(CreateUser())
 	app.migrations.add(CreateInAppPurchase())
